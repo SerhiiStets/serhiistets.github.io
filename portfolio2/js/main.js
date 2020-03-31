fec1a7ec8a33704da24fcc17ab4954c34dd7d66f
SerhiiStets <stets.serhii@gmail.com>
2020-04-01 02:47:58 +0300

    add another version of profile

---
+++ portfolio2/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+$(document).ready(function () {
+    var containerEl = document.querySelector('.container');
+
+    var mixer = mixitup(containerEl, {
+        animation: {
+            animateResizeContainer: false,
+            nudge: false,
+            clampHeight: false
+        }
+    });
+
+
+});
