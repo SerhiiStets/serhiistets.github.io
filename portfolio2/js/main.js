2473e8cc8df825438f8be687b49a73846d3015d7
SerhiiStets <stets.serhii@gmail.com>
2020-04-03 06:18:06 +0300

    fix some small bugs

--- portfolio2/js/main.js
+++ portfolio2/js/main.js
@@ -1,10 +1,38 @@
 "use strict";
 
 $(document).ready(function () {
-    
-    var containerEl = document.querySelector('.container');
 
+    /*
+    var win = $(this);
+    if (win.width() > 960) {
+
+        $('#show_all').addClass('active no-letterspace');
+
+    } else {
+        $('#show_all').removeClass('active no-letterspace');
+    }*/
+
+    var containerEl = $('.container');
+    
     var mixer = mixitup(containerEl, {
+        
+        callbacks: {
+            onMixClick: function(state, originalEvent) {
+                 var clicks = $(this).data('clicks');
+              if (clicks) {
+                  console.log("click")
+                  //var mixer2 = mixitup(containerEl);
+                  //$("#show_all_m").trigger('click');
+                 // odd clicks
+              } else {
+                  
+                 // even clicks
+              }
+              $(this).data("clicks", !clicks);
+
+            }
+        },
+        
         animation: {
             animateResizeContainer: false,
             nudge: false,
